80c8357a3c04543f3f1c4d594136e25722274be9
Kevin Kim <kevinfkim@gmail.com>
2020-06-16 20:10:05 +0000

    markup and styling for tweets article almost complete. just some last aesthetic changes.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,41 +1,13 @@
 $(document).ready(function() {
+  // NOTE: $.each(arr, (index, arrObj) => {}) is a jQuery style loop
   console.log("hello there");
-
-
-  // $(document).on("keypress", "#tweet-text", () => {
-  //   console.log("keypressss");
-  // });
-
-  // all keys, including modifyers
-  // $(document).on("keydown", "#tweet-text", () => {
-  //   console.log("keeyydonwwnnwn");
-  // });
-
-  // $(document).on("keyup", "#tweet-text", () => {
-  //   console.log("keyuppppp");
-  // });
-
-  // when element loses focues
-  // $(document).on("blur", "#tweet-text", () => {
-  //   console.log("bblluurrrrrr");
-  // });
-
-  // when input area has changed, registered upon blur
-  // $(document).on("change", "#tweet-text", () => {
-  //   console.log("cchhaannggee");
-  // });
-
-  // $(document).on("keypress", "#tweet-text", function() {
-  //   console.log(this);
-  //   console.log(this["id"]);
-  //   console.log($("textarea#tweet-text").val().length);
-  // });
 
   let totalChars = 140;
   let charLength = 0;
 
-  $(document).on("input", "#tweet-text", function() {
-    charLength = $("textarea#tweet-text").val().length;
+  // Character counter
+  $("#tweet-text").on("input", function() {
+    charLength = $(this).val().length; // element.target, instead of 'this', can be used
     $("output.counter").html(totalChars - charLength);
     if (charLength <= 140) {
       $("output.counter").replaceWith(`<output name="counter" class="counter" for="tweet-text">${totalChars - charLength}</output>`);
@@ -44,7 +16,9 @@ $(document).ready(function() {
     }
   });
 
-  // element.target, instead of 'this', can be used
-  //  $.each(arr, (index, arrObj) => {}) is a jQuery style loop
+  // Create Tweets
+
+
+
 
 });
