d66e68438a7962e046ab1afea4719f766c39427c
Kevin Kim <kevinfkim@gmail.com>
2020-06-16 16:21:41 +0000

    commit before implementing Tweet Creation feature.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,45 +1,50 @@
 $(document).ready(function() {
   console.log("hello there");
-});
 
-// $(document).on("keypress", "#tweet-text", () => {
-//   console.log("keypressss");
-// });
 
-// all keys, including modifyers
-// $(document).on("keydown", "#tweet-text", () => {
-//   console.log("keeyydonwwnnwn");
-// });
+  // $(document).on("keypress", "#tweet-text", () => {
+  //   console.log("keypressss");
+  // });
 
-// $(document).on("keyup", "#tweet-text", () => {
-//   console.log("keyuppppp");
-// });
+  // all keys, including modifyers
+  // $(document).on("keydown", "#tweet-text", () => {
+  //   console.log("keeyydonwwnnwn");
+  // });
 
-// when element loses focues
-// $(document).on("blur", "#tweet-text", () => {
-//   console.log("bblluurrrrrr");
-// });
+  // $(document).on("keyup", "#tweet-text", () => {
+  //   console.log("keyuppppp");
+  // });
 
-// when input area has changed, registered upon blur
-// $(document).on("change", "#tweet-text", () => {
-//   console.log("cchhaannggee");
-// });
+  // when element loses focues
+  // $(document).on("blur", "#tweet-text", () => {
+  //   console.log("bblluurrrrrr");
+  // });
 
-// $(document).on("keypress", "#tweet-text", function() {
-//   console.log(this);
-//   console.log(this["id"]);
-//   console.log($("textarea#tweet-text").val().length);
-// });
+  // when input area has changed, registered upon blur
+  // $(document).on("change", "#tweet-text", () => {
+  //   console.log("cchhaannggee");
+  // });
 
-let totalChars = 140;
-let charLength = 0;
+  // $(document).on("keypress", "#tweet-text", function() {
+  //   console.log(this);
+  //   console.log(this["id"]);
+  //   console.log($("textarea#tweet-text").val().length);
+  // });
 
-$(document).on("input", "#tweet-text", function() {
-  charLength = $("textarea#tweet-text").val().length;
-  $("output.counter").html(totalChars - charLength);
-  if (charLength <= 140) {
-    $("output.counter").replaceWith(`<output name="counter" class="counter" for="tweet-text">${totalChars - charLength}</output>`);
-  } else {
-    $("output.counter").replaceWith(`<output name="counter" class="counter" for="tweet-text" style="color:red">${totalChars - charLength}</output>`);
-  }
+  let totalChars = 140;
+  let charLength = 0;
+
+  $(document).on("input", "#tweet-text", function() {
+    charLength = $("textarea#tweet-text").val().length;
+    $("output.counter").html(totalChars - charLength);
+    if (charLength <= 140) {
+      $("output.counter").replaceWith(`<output name="counter" class="counter" for="tweet-text">${totalChars - charLength}</output>`);
+    } else {
+      $("output.counter").replaceWith(`<output name="counter" class="counter" for="tweet-text" style="color:red">${totalChars - charLength}</output>`);
+    }
+  });
+
+  // element.target, instead of 'this', can be used
+  //  $.each(arr, (index, arrObj) => {}) is a jQuery style loop
+
 });
