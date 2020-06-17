6d3da3543e4c2317235db3e1f9ff2143afa3b2bb
Kevin Kim <kevinfkim@gmail.com>
2020-06-17 21:13:21 +0000

    starting on form validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -172,6 +172,8 @@ $(document).ready(() => {
 
 
 
+
+
   // $("#more_tweets").click();
   // $("form").serialize();
   // or some sort of syntax like that, will take a form data and make it into string for data transfer, andthen will parse to json auto.
