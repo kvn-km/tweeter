2fdd8f7d8a333553d34b0e59b6130bfb24a0ac83
Kevin Kim <kevinfkim@gmail.com>
2020-06-17 16:46:02 +0000

    let's get started.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,38 @@
 
 
 
+function createTweetElement() {
+
+  const tArticle = $("<article>");
+  const tHeader = $("<header>");
+  const tSpan = $("<span>");
+
+
+
+}
+
+// const $tweet = $(`<article class="tweet">Hello world</article>`);
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+console.log($tweet); // to see what it looks like
+
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+
 // remember:
 
-// $("form").serialize(); 
+// $("form").serialize();
 // or some sort of syntax like that, will take a form data and make it into string for data transfer, andthen will parse to json auto.
