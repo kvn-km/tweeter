e07c48e7ab7845a3aa744c453ade1710dfbf040d
Kevin Kim <kvnkm@Kevins-MacBook-Pro.local>
2020-06-17 12:32:27 -0400

    commit before starting work for dynamic tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+// remember:
+
+// $("form").serialize(); 
+// or some sort of syntax like that, will take a form data and make it into string for data transfer, andthen will parse to json auto.
